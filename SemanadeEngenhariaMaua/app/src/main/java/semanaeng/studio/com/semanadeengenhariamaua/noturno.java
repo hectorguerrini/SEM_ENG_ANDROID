ea910c0924ff7b13e2cb31ff27ba15f9a5d7ca7e
Hector Guerrini Herrera <Hector Guerrini Herrera>
2017-04-02 00:41:18 -0300

    Aplicativo

---
+++ SemanadeEngenhariaMaua/app/src/main/java/semanaeng/studio/com/semanadeengenhariamaua/noturno.java
@@ -0,0 +1,39 @@
+package semanaeng.studio.com.semanadeengenhariamaua;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class noturno extends AppCompatActivity {
+
+    private ImageButton back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noturno);
+
+        TextView titulo = (TextView) findViewById(R.id.text_semana);
+
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/agency_fb.ttf");
+        titulo.setTypeface( font );
+
+        back = (ImageButton) findViewById(R.id.button_back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(noturno.this, MainActivity.class));
+            }
+        });
+
+
+
+    }
+}
