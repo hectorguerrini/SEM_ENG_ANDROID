572f25f2a2f7117259b8c339c64a309d81d43795
HectorGuerrini <hectorumaga@hotmail.com>
2017-04-13 18:06:12 -0300

    Activity mapa

--- SemanadeEngenhariaMaua/app/src/main/java/semanaeng/studio/com/semanadeengenhariamaua/MainActivity.java
+++ SemanadeEngenhariaMaua/app/src/main/java/semanaeng/studio/com/semanadeengenhariamaua/MainActivity.java
@@ -1,19 +1,24 @@
 package semanaeng.studio.com.semanadeengenhariamaua;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuItemWrapperICS;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 @SuppressWarnings("ALL")
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Button diurno;
     private Button noturno;
@@ -31,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_barra_de_navegacao);
 
         toolbar = (Toolbar) findViewById(R.id.toolbarPrincipal);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 
         diurno = (Button) findViewById(R.id.button_di);
@@ -42,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         sobre = (Button) findViewById(R.id.button_sobre);
         TextView titulo = (TextView) findViewById(R.id.text_semana);
         nDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/agency_fb.ttf");
         titulo.setTypeface( font );
@@ -102,6 +107,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        navigationView.setNavigationItemSelectedListener(this);
 
+    }
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_notificacao) {
+            // Handle the camera action
+        } else if (id == R.id.nav_mapa) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
