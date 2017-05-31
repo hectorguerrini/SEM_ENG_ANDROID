267b9a2c891abfdcda4fdd21c44b0fab8449541d
HectorGuerrini <hectorumaga@hotmail.com>
2017-05-31 14:39:13 -0300

    login.. cadastro...

--- SemanadeEngenhariaMaua/app/src/main/java/semanaeng/studio/com/semanadeengenhariamaua/activity/login.java
+++ SemanadeEngenhariaMaua/app/src/main/java/semanaeng/studio/com/semanadeengenhariamaua/activity/login.java
@@ -1,24 +1,24 @@
 package semanaeng.studio.com.semanadeengenhariamaua.activity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import semanaeng.studio.com.semanadeengenhariamaua.R;
 
 public class login extends AppCompatActivity {
     private Button back;
+    private Button cad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        cad = (Button) findViewById(R.id.button_ir_cadastro);
 
         back = (Button) findViewById(R.id.button_back);
 
@@ -27,6 +27,12 @@ public class login extends AppCompatActivity {
             public void onClick(View v) {
                 onBackPressed();
 
+            }
+        });
+        cad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, cadastro.class));
             }
         });
 
